1dbf9a40e2920e72134e44edad28ee7ebd804f01
JesseB <xmzedu@gmail.com>
2022-03-22 20:51:02 +0800

    update middleware

--- __json_sever__/middleware.js
+++ __json_sever__/middleware.js
@@ -1,12 +1,13 @@
 module.exports = (req, res, next) => {
-  if (req.method === "post" && req.path === "/login") {
+  console.log("login中间件生效");
+  if (req.method === "POST" && req.path === "/login") {
     //为什么path只等于login？
     if (req.body.username === "jesseb" && req.body.password === "123456") {
-      return res.state(200).json({
+      return res.status(200).json({
         user: { token: 123 },
       });
     } else {
-      return res.state(400).json({ message: "用户名或密码错误" });
+      return res.status(400).json({ message: "用户名或密码错误" });
     }
   }
   next();
